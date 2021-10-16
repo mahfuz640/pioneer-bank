4fdd36c84ff53221275ab7ad5a5cefc4ad1e40bc
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-16 12:44:01 +0600

    js update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// login button handle
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function() {
     const loginArea = document.getElementById("login-area")
