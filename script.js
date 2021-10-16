04d209eccb14fed98863c9170613650f7665b0e3
rabbi640 <69029846+rabbi640@users.noreply.github.com>
2021-10-16 20:29:49 +0600

    js update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // login button handle
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function() {
-    const loginArea = document.getElementById("login-area")
-    loginArea.style.display = "none";
+        const loginArea = document.getElementById("login-area")
+        loginArea.style.display = "none";
 
-    const transactionArea = document.getElementById("transaction-area");
-    transactionArea.style.display = "block";
-})
+        const transactionArea = document.getElementById("transaction-area");
+        transactionArea.style.display = "block";
+    })
+    // deposite button event handler
+const depositeBtn = document.getElementById("addDeposit")
